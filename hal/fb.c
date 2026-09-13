70dd2f86545150bc31ba39e340f14f4f2ed434b1
akrom <akrommuhammadyusuf@gmail.com>
2026-09-13 16:42:35 +0700

    hal: add VBE linear framebuffer driver and text console stubs

---
+++ hal/fb.c
@@ -0,0 +1,37 @@
+#include "fb.h"
+#include "serial.h"
+
+static boot_info_t current_fb_info;
+static uint8_t fb_active = 0;
+
+void fb_init(boot_info_t* info) {
+    if (!info) {
+        fb_active = 0;
+        return;
+    }
+
+    if (info->magic == BOOT_INFO_MAGIC && info->is_graphical) {
+        current_fb_info = *info;
+        fb_active = 1;
+        serial_puts("[Akryon HAL] VBE Linear Framebuffer initialized: ");
+        if (info->fb_width == 1024) {
+            serial_puts("1024x768");
+        } else if (info->fb_width == 800) {
+            serial_puts("800x600");
+        } else {
+            serial_puts("Custom Res");
+        }
+        serial_puts(" @ 32bpp\n");
+    } else {
+        fb_active = 0;
+        serial_puts("[Akryon HAL] VBE not available, fallback to VGA text mode.\n");
+    }
+}
+
+boot_info_t* fb_get_info(void) {
+    return &current_fb_info;
+}
+
+uint8_t fb_is_active(void) {
+    return fb_active;
+}
---
+++ hal/fb.h
@@ -0,0 +1,23 @@
+#ifndef AKRYON_HAL_FB_H
+#define AKRYON_HAL_FB_H
+
+#include "types.h"
+
+#define BOOT_INFO_MAGIC 0x414B5259 // 'AKRY'
+
+typedef struct {
+    uint32_t magic;
+    uint32_t fb_addr;
+    uint16_t fb_width;
+    uint16_t fb_height;
+    uint16_t fb_pitch;
+    uint8_t  fb_bpp;
+    uint8_t  is_graphical;
+    uint8_t  reserved[2];
+} __attribute__((packed)) boot_info_t;
+
+void fb_init(boot_info_t* info);
+boot_info_t* fb_get_info(void);
+uint8_t fb_is_active(void);
+
+#endif // AKRYON_HAL_FB_H
