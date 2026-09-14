0e1b92f876b6298cfef99b9b034438159f0ed085
akrom <akrommuhammadyusuf@gmail.com>
2026-09-14 21:44:53 +0700

    hal: optimize memset, memcpy, and memmove with 32-bit word fast path

--- hal/string.c
+++ hal/string.c
@@ -1,16 +1,41 @@
 #include "types.h"
 
 void* memset(void* dest, int val, size_t count) {
-    uint8_t* temp = (uint8_t*)dest;
+    uint8_t* dp = (uint8_t*)dest;
+    uint8_t v = (uint8_t)val;
+    if (count >= 16 && ((uintptr_t)dp & 3) == 0) {
+        uint32_t v32 = (uint32_t)v | ((uint32_t)v << 8) | ((uint32_t)v << 16) | ((uint32_t)v << 24);
+        uint32_t* dp32 = (uint32_t*)dp;
+        size_t words = count / 4;
+        for (size_t i = 0; i < words; i++) {
+            dp32[i] = v32;
+        }
+        for (size_t i = words * 4; i < count; i++) {
+            dp[i] = v;
+        }
+        return dest;
+    }
     for (size_t i = 0; i < count; i++) {
-        temp[i] = (uint8_t)val;
+        dp[i] = v;
     }
     return dest;
 }
 
 void* memcpy(void* dest, const void* src, size_t count) {
-    const uint8_t* sp = (const uint8_t*)src;
     uint8_t* dp = (uint8_t*)dest;
+    const uint8_t* sp = (const uint8_t*)src;
+    if (count >= 16 && (((uintptr_t)dp | (uintptr_t)sp) & 3) == 0) {
+        uint32_t* dp32 = (uint32_t*)dp;
+        const uint32_t* sp32 = (const uint32_t*)sp;
+        size_t words = count / 4;
+        for (size_t i = 0; i < words; i++) {
+            dp32[i] = sp32[i];
+        }
+        for (size_t i = words * 4; i < count; i++) {
+            dp[i] = sp[i];
+        }
+        return dest;
+    }
     for (size_t i = 0; i < count; i++) {
         dp[i] = sp[i];
     }
@@ -21,10 +46,34 @@ void* memmove(void* dest, const void* src, size_t count) {
     uint8_t* dp = (uint8_t*)dest;
     const uint8_t* sp = (const uint8_t*)src;
     if (dp < sp) {
+        if (count >= 16 && (((uintptr_t)dp | (uintptr_t)sp) & 3) == 0) {
+            uint32_t* dp32 = (uint32_t*)dp;
+            const uint32_t* sp32 = (const uint32_t*)sp;
+            size_t words = count / 4;
+            for (size_t i = 0; i < words; i++) {
+                dp32[i] = sp32[i];
+            }
+            for (size_t i = words * 4; i < count; i++) {
+                dp[i] = sp[i];
+            }
+            return dest;
+        }
         for (size_t i = 0; i < count; i++) {
             dp[i] = sp[i];
         }
     } else if (dp > sp) {
+        if (count >= 16 && (((uintptr_t)dp | (uintptr_t)sp) & 3) == 0) {
+            uint32_t* dp32 = (uint32_t*)dp;
+            const uint32_t* sp32 = (const uint32_t*)sp;
+            size_t words = count / 4;
+            for (size_t i = count; i > words * 4; i--) {
+                dp[i - 1] = sp[i - 1];
+            }
+            for (size_t i = words; i > 0; i--) {
+                dp32[i - 1] = sp32[i - 1];
+            }
+            return dest;
+        }
         for (size_t i = count; i > 0; i--) {
             dp[i - 1] = sp[i - 1];
         }
